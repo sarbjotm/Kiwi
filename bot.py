118cc95f82fa62ac1f345e6aaca360aaf404b852
Unicue <Unicue@users.noreply.github.com>
2020-11-06 18:44:02 -0800

    Added Shorthand for Fireworks (fw)

--- bot.py
+++ bot.py
@@ -116,15 +116,6 @@ async def fireworks(ctx, *, message):
         else:
             fireworkString += (messageList[i] + emoji)
     await ctx.send(f"{fireworkString}")
-    
-@client.command(aliases = ["sp","space","spaces"])
-async def spaced(ctx, *, message):
-    noSpaceString = message.replace(" ", "")
-    spacedString = ""
-    for i in range(0, len(noSpaceString)):
-        spacedString += noSpaceString[i]
-        spacedString += " "
-    await ctx.send(f"{spacedString}")
 
 f = open("specialCode.txt", "r")
 Token = str(f.readline()).strip('\n')
