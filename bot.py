7e08c5d2f2767292125f38cd4ee1a2ec3f595b6d
Sarbjot <sarbjotmann98@gmail.com>
2020-12-04 21:26:26 -0800

    Fixed ,collect bug and added new help command

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import random
 from discord.ext import commands, tasks
 
 client = commands.Bot(command_prefix = ',')
+client.remove_command('help')
 
 @client.command()
 async def load(ctx,extension):
@@ -22,13 +23,25 @@ for filename in os.listdir('./cogs'):
 async def on_ready():
     print("Bot is Ready")
 
-@commands.command()
-@commands.cooldown(1,1440, commands.BucketType.user)
-async def collect(self,ctx):
-    roles = ['Santa Dodo', 'Elf Dodo', 'Frosty The Snow Dodo', 'Gift Dodo', 'Grinch Dodo']
-    roleAssign = random.choices(roles, weights = [5,5,10,30,50])
-    await ctx.send(f'You have drawn the {roleAssign[0]} role! Your next chance to role is in 24 hours')
-    await client.add_roles(ctx.message.author, roleAssign[0])
+@client.command(pass_context=True)
+async def help(ctx):
+    embed=discord.Embed(title="Help Command", description="List of all commands", color=0x59cbf0)
+    embed.add_field(name=",waves", value="Wave at a user", inline=False)
+    embed.add_field(name=",wavesRole @role", value="Waves at a role", inline=False)
+    embed.add_field(name=",hug @user", value="Give @user a hug", inline=False)
+    embed.add_field(name=",hugsRole @role", value="Grouphug with @role members", inline=False)
+    embed.add_field(name=",8ball question", value="Gives the user an answer to their question", inline=False)
+    embed.add_field(name=",coinflip", value="Heads or Tails", inline=False)
+    embed.add_field(name=",collect", value="Collect one of the time-limited event roles", inline=False)
+    embed.add_field(name=",trade role1 @user role2", value="Trade role1 with another users role 2", inline=False)
+    embed.add_field(name=",randomnumber a b", value="Displays a random number between a and b inclusvely", inline=False)
+    embed.add_field(name=",fw message", value="Seperate the message with sparkles", inline=False)
+    embed.add_field(name=",spaced message", value="Add spaces into the message", inline=False)
+    embed.add_field(name=",spongebob message", value="Convert the message into Spongebob Meme Format", inline=False)
+    embed.add_field(name=",banAlly", value="Speak the truth", inline=False)
+    embed.add_field(name=",ping", value="Pong", inline=False)
+    embed.add_field(name=",help", value="Display this message", inline=False)
+    await ctx.send(embed=embed)
 
 # @client.event
 # async def on_member_join(member):
--- cogs/util.py
+++ cogs/util.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import random
+import asyncio
+
 
 #Utils
 class Utilities(commands.Cog):
@@ -10,14 +12,44 @@ class Utilities(commands.Cog):
     @commands.command()
     async def ping(self,ctx):
         await ctx.send(f'Pong!')
-    
+
     @commands.command()
-    @commands.cooldown(1,86400, commands.BucketType.user)
+    async def trade(self,ctx,role: discord.Role,member : discord.Member,roleOther: discord.Role):
+        if ((role in ctx.message.author.roles) and (roleOther in member.roles)):    
+            await ctx.send(f'{ctx.message.author.mention} wants to trade {role} to {member.mention} for {roleOther}')
+            await ctx.send(f'{member.mention} do you accept? (Yes/No). You have 30 seconds to accept')
+            try:
+                msg = await self.client.wait_for(
+                    "message",
+                    timeout = 30,
+                    check=lambda message: message.author == member \
+                        and message.channel == ctx.channel 
+                )
+                msg = msg.content.strip().lower()
+                if msg == 'yes':
+                    await ctx.message.author.add_roles(roleOther)
+                    await member.add_roles(role)
+                    await ctx.message.author.remove_roles(role)
+                    await member.remove_roles(roleOther)
+                    await ctx.send(f'Trade Completed!')
+                elif msg == 'no':
+                    await ctx.send(f'Trade Rejected!')
+                else:
+                    await ctx.send(f'Invalid input. Trade Rejected')
+
+            except asyncio.TimeoutError:
+                await ctx.send(f'Cancelling due to time out ') 
+        else:
+            await ctx.send(f'You or the user you want to trade with does not have the role listed in the trade')   
+
+    @commands.command()
+    @commands.cooldown(1,43200, commands.BucketType.user)
     async def collect(self,ctx):
-        roles = ['Santa Dodo', 'Elf Dodo', 'Frosty The Snow Dodo', 'Gift Dodo', 'Grinch Dodo']
-        roleAssign = random.choices(roles, weights = [5,5,10,30,50])[0]
-        await ctx.message.author.add_roles(roleAssign)
-        await ctx.send(f'You have drawn the {roleAssign[0]} role! Your next chance to role is in 24 hours')
+        rolesList = ['Santa Dodo', 'Elf Dodo', 'Frosty The Snowman Dodo', 'Gift Dodo', 'Grinch Dodo']
+        roleAssign = random.choices(rolesList, weights = [5,5,10,30,50])[0]
+        role = discord.utils.get(ctx.guild.roles, name= str(roleAssign))
+        await ctx.message.author.add_roles(role)
+        await ctx.send(f'You have drawn the {role} role! Your next chance to role is in 12 hours')
 
     @collect.error
     async def collect_error(self,ctx,error):
@@ -26,8 +58,11 @@ class Utilities(commands.Cog):
             hours = seconds // 3600
             seconds %= 3600
             minutes = seconds // 60
-            await ctx.send(f'Try again in {hours:.2f} hours and {minutes:.2f} minutes')
-             
+            if hours != 0:
+                await ctx.send(f'Try again in {hours} hours and {minutes} minutes')
+            else:
+                await ctx.send(f'Try again in {minutes} minutes')
+
 #setup
 def setup(client):
     client.add_cog(Utilities(client))
