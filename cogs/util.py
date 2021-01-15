db3cc758bc1eede5d6f79981b9a1bc249fe5877d
Sarbjot <sarbjotmann98@gmail.com>
2021-01-15 01:20:01 -0800

    Trying to find where users are duplicated

--- cogs/util.py
+++ cogs/util.py
@@ -76,6 +76,7 @@ class Utilities(commands.Cog):
         await ctx.message.author.add_roles(role)
         await ctx.send(f'You have drawn the {role} role! To activate it use the ,activate \"{role}\" command. Your next chance to roll is in 12 hours')
         roleAssign.split()
+        print(f"roleAssign[1]")
         try:
             c.execute(f"""UPDATE dodos
             SET {roleAssign[1]} = 767986908
