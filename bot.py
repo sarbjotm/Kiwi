c1c8e1a831397cc3fb56adf4025c8ba7685ee74a
Sarbjot <sarbjotmann98@gmail.com>
2021-01-17 18:26:16 -0800

    Troubleshooting none type

--- bot.py
+++ bot.py
@@ -6,15 +6,14 @@ import sqlite3
 from discord.ext import commands, tasks
 
 #TODO: Have bot add members to database with according values when it is run
-intents = discord.Intents.default()
-intents.members = True
+intents = discord.Intents(messages = True, members = True)
 client = commands.Bot(command_prefix = ',',intents=intents)
 client.remove_command('help')
 
 conn = sqlite3.connect('members.db')
 c = conn.cursor()
 guild = client.get_guild('744817281871249428')
-memberList = guild.members
+print(f"{guild}")
 rolesList = ['Dodo Red','Dodo Orange','Dodo Yellow','Dodo Green','Dodo Teal','Dodo Copyright','Dodo Bluev2','Dodo Blue','Dodo Purplev2','Dodo Purple','Dodo Pinkv2','Dodo Pink']
 
 @client.command()
