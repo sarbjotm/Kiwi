30aa2e585ed90b97a0173b015634a8383229159e
Sarbjot <sarbjotmann98@gmail.com>
2021-02-11 22:51:19 -0800

    Changed activate to array so no longer case senestive

--- cogs/roles.py
+++ cogs/roles.py
@@ -200,11 +200,9 @@ class Utilities(commands.Cog):
 
 
     @commands.command()
-    async def activate(self,ctx,role: discord.Role):
-        role = str(role)
-        role = role.split(" ")
+    async def activate(self,ctx,*role):
         role = role[0][0].upper() + role[0][1:] + " " + role[1][0].upper()+role[1][1:]
-        role = discord.utils.get(ctx.guild.roles, name=role)
+        role = discord.utils.get(ctx.guild.roles, name = role)
         if ((str(role) not in rolesList)):
             await ctx.send("Only can activate collected Colour Roles.")
             await ctx.send("Make sure you have provided the correct arguments. Roles are case senestive")
