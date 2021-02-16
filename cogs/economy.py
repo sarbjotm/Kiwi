ae5080d999cf5ff402ae725f9eb71089055a9027
Sarbjotm <sarbjotmann98@gmail.com>
2021-02-15 17:37:25 -0800

    Trial Run Passed

--- cogs/economy.py
+++ cogs/economy.py
@@ -90,14 +90,13 @@ class Economy(commands.Cog):
             
     @commands.command(aliases = ['shopinfo'])
     async def shop(self,ctx):
-        await ctx.send("Trial Run")
-        # moneySymbol = discord.utils.get(ctx.message.guild.emojis, name='money')
-        # embed=discord.Embed(title="Kiwi Shop" , color=0xe392fe)
-        # embed.set_thumbnail(url= "https://sdl-stickershop.line.naver.jp/products/0/0/1/1233993/android/stickers/9496426.png;compress=true")
-        # for i in range(0,len(rolesList)):
-        #     embed.add_field(name=rolesList[i], value="5000 " + moneySymbol, inline=True)
-        # await ctx.message.delete(delay = 0)
-        # await ctx.send(embed=embed)
+        moneySymbol = discord.utils.get(ctx.message.guild.emojis, name='money')
+        embed=discord.Embed(title="Kiwi Shop" , color=0xe392fe)
+        embed.set_thumbnail(url= "https://sdl-stickershop.line.naver.jp/products/0/0/1/1233993/android/stickers/9496426.png;compress=true")
+        for i in range(0,len(rolesList)):
+            embed.add_field(name=rolesList[i], value="5000 " + moneySymbol, inline=True)
+        await ctx.message.delete(delay = 0)
+        await ctx.send(embed=embed)
 
 
 
