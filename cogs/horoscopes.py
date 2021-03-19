639e2f4396a4aa818b6924f972b47ab126fdcf6b
Sarbjotm <sarbjotmann98@gmail.com>
2021-03-19 13:24:34 -0700

    Changed embeds to be fancier

--- cogs/horoscopes.py
+++ cogs/horoscopes.py
@@ -36,7 +36,9 @@ class Horoscope(commands.Cog):
             source = requests.get(f'https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign={number}').text
             soup = BeautifulSoup(source,'lxml')
             todayHoroscope = soup.p.text
-            await ctx.send(f"{todayHoroscope}")
+            embed=discord.Embed(title="Daily Horoscope", color=0x968cec)
+            embed.set_footer(text=f'{todayHoroscope}')
+            await ctx.send(embed=embed)
         else:
             await ctx.send("Not a valid zodiac sign")
     
@@ -46,15 +48,7 @@ class Horoscope(commands.Cog):
         channel = ctx.guild.get_channel(800965152132431892)
         await ctx.send("Error Occured. Syntax for this command is: **,hide zodiac_sign**")
         await channel.send(f"{ctx.message.author} experienced a error using horoscope")  
-    
-    @commands.command()
-    async def amandertestingembeds(self,ctx):
-        embed=discord.Embed(title="Daily Horoscope", color=0x968cec)
-        embed.set_footer(text="Unmarried people may run into differences with loved ones over future planning. They need to refrain from stretching things too far. Married couples may enjoy together. On the family front, things seem good. If you are in a meaningful relationship, you might have some disagreements. Do try asserting your view non-aggressively. Try to maintain peace and harmony in the relationship. On the financial front, things may be strong, and at the end of the week, you may have a chance to gain monetarily. Businessmen may strike a big-ticket deal while salaried individuals may get useful guidance from their superiors. Students pursuing graduation may be well-focused while studying. They may make good progress. Students pursuing post-graduation may get help from their professors. Their progress might be good too. On the health front, everything remains good. Beware of an upper-body injury. Perform some exercises in the morning to remain in good health.")
-        await ctx.send(embed=embed)
-
-
-      
+          
 
 def setup(client):
     client.add_cog(Horoscope(client))
