8ff355e6f5c610bb66d3dfcd6b92a3e070ae8c72
Sarbjotm <sarbjotmann98@gmail.com>
2021-04-02 00:58:16 -0700

    Added about to help command + small changes in formatting

--- bot.py
+++ bot.py
@@ -109,18 +109,18 @@ async def help(ctx):
     embed.add_field(name="Astrology", value="**,horoscope zodiac** - Get your daily horoscope reading" , inline=True)
     embed.add_field(name="Weather", value="**,weather city** - Get your daily weather forecast" , inline=True)
     embed.add_field(name="Other", value="**,randomnumber a b ** - display rng [a,b] \n **,kittyclap** - send a kittyclap", inline=True)
-    embed.add_field(name="Help Commands", value="**,help** - shows this message \n**,ping** - check if kiwi is still up", inline=True)
+    embed.add_field(name="Help Commands", value="**,about** - display information about kiwi \n **,help** - shows this message \n**,ping** - check if kiwi is still up", inline=True)
     await ctx.send(embed=embed)
 
 
 @client.command(pass_context=True)
 async def about(ctx):
-    embed = discord.Embed(title="About", description = "Kiwi is one of SFU Dodo Club's mascots, and is also our main Discord bot. Kiwi is constantly being updated and is maintend by myself. Kiwi is currently being hosted on Heroku and has an MySQL Database connected to it.",color=0x66abf9)    
+    embed = discord.Embed(title="About", description = "Kiwi is one of SFU Dodo Club's mascots, and is also our main Discord bot. Kiwi is constantly being updated and is maintend by myself. Kiwi is currently being hosted on Heroku under a Hobby Plan and has an MySQL Database connected to it.",color=0x66abf9)    
     embed.set_author(name="Amander", icon_url="https://i.pinimg.com/originals/81/d7/d0/81d7d0dac44a4689449748532aac9f37.png")
-    embed.add_field(name="Discord", value="Amander#5186", inline=True)
+    embed.add_field(name="Discord", value="<@264645255427522560>", inline=True)
     embed.add_field(name="Email", value=f"{os.environ['EMAIL']}", inline=True)
     embed.add_field(name="Github", value='[https://github.com/sarbjotm/Kiwi](https://github.com/sarbjotm/Kiwi)', inline=False)
-    embed.add_field(name="Donations", value='I support this bot personally. Donations will help offset my costs of running and maintaining the bot. \n \n **E-Transfer**: Email above', inline=False)
+    embed.add_field(name="Donations", value='I fianace this bot personally. Donations will help offset my costs of running and maintaining the bot. \n \n **E-Transfer**: Email above', inline=False)
     await ctx.send(embed=embed)
 
 
