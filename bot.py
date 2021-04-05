8dda4806ce36e05cd091346364c3a2efc0ee4caf
Sarbjotm <sarbjotmann98@gmail.com>
2021-04-04 18:12:54 -0700

    Added paypal link

--- bot.py
+++ bot.py
@@ -115,12 +115,13 @@ async def help(ctx):
 
 @client.command(pass_context=True)
 async def about(ctx):
+    paypal = {os.environ['PAYPAL']}
     embed = discord.Embed(title="About", description = "Kiwi is one of SFU Dodo Club's mascots, and is also our main Discord bot. Kiwi is constantly being updated and is maintend by myself. Kiwi is currently being hosted on Heroku under a Hobby Plan and has an MySQL Database connected to it.",color=0x66abf9)    
     embed.set_author(name="Amander", icon_url="https://i.pinimg.com/originals/81/d7/d0/81d7d0dac44a4689449748532aac9f37.png")
     embed.add_field(name="Discord", value="<@264645255427522560>", inline=True)
     embed.add_field(name="Email", value=f"{os.environ['EMAIL']}", inline=True)
     embed.add_field(name="Github", value='[https://github.com/sarbjotm/Kiwi](https://github.com/sarbjotm/Kiwi)', inline=False)
-    embed.add_field(name="Donations", value='I fianace this bot personally. Donations will help offset my costs of running and maintaining the bot. \n \n **E-Transfer**: Email above', inline=False)
+    embed.add_field(name="Donations", value=f'I fianace this bot personally. Donations will help offset my costs of running and maintaining the bot. \n \n **E-Transfer**: Email above \nPaypal:[{paypal}]({paypal}))', inline=False)
     await ctx.send(embed=embed)
 
 
