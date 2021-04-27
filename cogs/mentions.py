dc38c6885169ad08cd3b780164f28fb36df4210f
Sarbjotm <sarbjotmann98@gmail.com>
2021-04-26 23:59:14 -0700

    Added lookup command for other users

--- cogs/mentions.py
+++ cogs/mentions.py
@@ -1,6 +1,13 @@
 import discord
 import asyncio
 from discord.ext import commands
+import mysql
+
+
+#Roles List 
+activateRoles = ['Red','Orange','Yellow','Green','Teal','Copyright','Cyan','Blue','Grape','Purple','Rose','Pink','Salmon','Spring','Matcha','Mint','Ice','Bbblu','Lavender','Special']
+
+
 
 #Mentions
 class Interactions(commands.Cog):
@@ -58,6 +65,52 @@ class Interactions(commands.Cog):
     @commands.command(aliases = ["bringpeace"])
     async def banAlly(self,ctx):
         await ctx.send('Yes let us ban Ally!! Let us also ban Kyle!!')
+
+    @commands.command()
+    async def info(self,ctx, member : discord.Member):
+        db = mysql.connector.connect(
+            host= os.environ['HOST'],
+            user = os.environ['USER'],
+            password = os.environ['PASSWORD'],
+            database = os.environ['DATABASE']
+        )
+        c = db.cursor() 
+        embedDescription = "**Role Information** \n"
+        user = str(ctx.message.author)
+        
+        for role in activateRoles:
+            c.execute(f"""SELECT {role}
+                        FROM dodos
+                        WHERE id = {ctx.message.author.id}
+
+        """)
+            roleCount = ''.join(map(str,c.fetchall()[0]))
+            embedDescription = embedDescription + roleCount + " Dodo " + role  + " roles" + "\n"
+        embed=discord.Embed(title= user + "'s Information", description = embedDescription, color=0xe392fe)    
+        embed.set_author(name= ctx.message.author, icon_url=ctx.member.avatar_url)
+        
+        c.execute(f"""SELECT money
+                        FROM dodos
+                        WHERE id = {ctx.message.author.id}
+
+        """)
+        money = ''.join(map(str,c.fetchall()[0]))
+        c.execute(f"""SELECT birthday
+                        FROM dodos
+                        WHERE id = {ctx.message.author.id}
+
+        """)
+        birthday = ''.join(map(str,c.fetchall()[0]))
+        embed.add_field(name=f"Money Balance", value=f"{money}" , inline=True)
+        if(birthday == '0000'):
+            embed.add_field(name=f"Birthday", value=f"N/A" , inline=True)
+        else:
+            embed.add_field(name=f"Birthday", value=f"{birthday}" , inline=True)
+        c.close()
+        db.close()
+
+
+        
 
 def setup(client):
     client.add_cog(Interactions(client))
