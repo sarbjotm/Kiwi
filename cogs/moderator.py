cddccfe6c022ddaefa9b5205e196c058c5077074
Sarbjotm <sarbjotmann98@gmail.com>
2021-05-05 21:21:27 -0700

    createrole command

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -2,12 +2,28 @@ import discord
 from discord.ext import commands
 import random
 import os
+from discord.ext.commands import has_permissions
+import re
+
 pollOptions = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"]
 
 
 class Moderator(commands.Cog):
     def __init__(self, client):
         self.client = client
+
+    @has_permissions(manage_roles=True)
+    @commands.command()
+    async def createrole(self, ctx, colour, *, role):
+        regex = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$"
+        p = re.compile(regex)
+        if re.search(p, str(colour)):
+            guild = ctx.guild
+            colour_of_role = '0x'+str(colour)
+            await guild.create_role(name=role, colour=discord.Colour(colour_of_role))
+            await ctx.send(f"{role} created with colour code {colour_of_role}")
+        else:
+            await ctx.send("Enter a valid hex colour code")
 
 
 def setup(client):
