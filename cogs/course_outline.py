d2b70f0eef893fc2107cb33c3acfcca8e4cd5621
Sarbjot Mann <44513277+sarbjotm@users.noreply.github.com>
2021-09-12 19:02:05 -0700

    Finished Command. Formatting updated

--- cogs/course_outline.py
+++ cogs/course_outline.py
@@ -18,54 +18,52 @@ class Outline(commands.Cog):
         course_number = course[len(course)-3:]
         
         try:
-            source = requests.get(f'http://www.sfu.ca/outlines.html?2021/fall/{course_name}/{course_number}/{section}',timeout=5).text
-
+            source = requests.get(f'http://www.sfu.ca/students/calendar/2021/fall/courses/{course_name}/{course_number}', timeout = 5).text
+            
         except Timeout:
             await ctx.send("Error accessing server data, please try again later")
             
         else:
-            soup = BeautifulSoup(source, 'lxml')
-            embed_description = ""
-            embed_title = course_name.upper()+str(course_number) + " " + section + " - "
-            courses_name = soup.find("h2", {"id": "title"}).text
-            courses_name = courses_name.split()
-            for i in range(0,len(courses_name)):
-                embed_title = embed_title + courses_name[i] + " "
-            
-            time = soup.find("li", {"class": "course-times"}).text
-            time = time.split()
-            prereq = soup.find("li", {"class": "prereq"}).text
-            prereq = prereq.split()
-            instructor = soup.find("li", {"class": "instructor"}).text
-            instructor = instructor.split()
-
-            for i in range(0, len(time)):
-                embed_description = embed_description + time[i] + " "
-                if time[i] == "Location:" or time[i] == "Burnaby" or time[i] == "Surrey":
-                        embed_description = embed_description + "\n"
-            
-            embed_description = embed_description + "\n \n"
-
-            for i in range(0, len(prereq)):
-                embed_description = embed_description + prereq[i] + " "
-            
-            embed_description = embed_description + "\n \n"
-
-            for i in range(0, len(instructor)):
-                embed_description = embed_description + instructor[i] + " "
-            
-            
+            soup = BeautifulSoup(source,'lxml')
+            course_description = soup.find_all('p')
+            embed_description = course_description[1].get_text() + "\n \n"
+        
             try:
-                source = requests.get(f'http://www.sfu.ca/students/calendar/2021/fall/courses/{course_name}/{course_number}', timeout = 5).text
+                source = requests.get(f'http://www.sfu.ca/outlines.html?2021/fall/{course_name}/{course_number}/{section}',timeout=5).text
 
             except Timeout:
                 await ctx.send("Error accessing server data, please try again later")
-            
+
             else:
-                soup = BeautifulSoup(source,'lxml')
-                course_description = soup.find_all('p')
-                #print(course_description[1].get_text()) 
-                embed_description = embed_description + "\n" + course_description[1].get_text()
+                soup = BeautifulSoup(source, 'lxml')
+                embed_title = course_name.upper()+str(course_number) + " " + section + " - "
+                courses_name = soup.find("h2", {"id": "title"}).text
+                courses_name = courses_name.split()
+                for i in range(0,len(courses_name)):
+                    embed_title = embed_title + courses_name[i] + " "
+
+                time = soup.find("li", {"class": "course-times"}).text
+                time = time.split()
+                prereq = soup.find("li", {"class": "prereq"}).text
+                prereq = prereq.split()
+                instructor = soup.find("li", {"class": "instructor"}).text
+                instructor = instructor.split()
+
+                embed_description = embed_description + "\n \n"
+
+                for i in range(0, len(prereq)):
+                    embed_description = embed_description + prereq[i] + " "
+
+                embed_description = embed_description + "\n \n"
+
+                for i in range(0, len(instructor)):
+                    embed_description = embed_description + instructor[i] + " "
+
+                for i in range(0, len(time)):
+                    embed_description = embed_description + time[i] + " "
+                    if time[i] == "Location:" or time[i] == "Burnaby" or time[i] == "Surrey":
+                            embed_description = embed_description + "\n"
+
                 embed_description = embed_description.strip(" ")
                 embed = discord.Embed(title=embed_title,description=embed_description, color=0xa6192e)
                 await ctx.send(embed=embed)
