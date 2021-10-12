0d0e49f5bfd85930deb80d790cbbbb7f2a060f03
Sarbjotm <sarbjotmann98@gmail.com>
2021-10-11 19:41:39 -0700

    course_outline.py changed to nextcord library

--- cogs/course_outline.py
+++ cogs/course_outline.py
@@ -1,10 +1,11 @@
-import discord
-from discord.ext import commands
+import nextcord
+from nextcord.ext import commands
 import requests
 from requests.exceptions import Timeout
 from bs4 import BeautifulSoup
 
 from myconstants import zodiacSigns, zodiacAvatars
+
 
 # Horoscope Alterations
 class Outline(commands.Cog):
@@ -14,66 +15,70 @@ class Outline(commands.Cog):
     # Horoscope function - get daily horoscope from horoscope.com based from zodiac sign
     @commands.command(aliases=['sfu'])
     @commands.guild_only()
-    async def outline(self, ctx, course, section = "D100"):
-        course_name = course[0:len(course)-3]
-        course_number = course[len(course)-3:]
-        
+    async def outline(self, ctx, course, section="D100"):
+        course_name = course[0:len(course) - 3]
+        course_number = course[len(course) - 3:]
+
         try:
-            source = requests.get(f'http://www.sfu.ca/students/calendar/2021/fall/courses/{course_name.lower()}/{course_number}', timeout = 5)
-            
+            source = requests.get(
+                f'http://www.sfu.ca/students/calendar/2021/fall/courses/{course_name.lower()}/{course_number}',
+                timeout=5)
+
         except Timeout:
             await ctx.send("Error accessing server data, please try again later")
-            
+
         else:
             if source.status_code == 404:
-                embed = discord.Embed(title="Course Not Found",description="404 Error: This course does not exist", color=0xa6192e)
+                embed = nextcord.Embed(title="Course Not Found", description="404 Error: This course does not exist",
+                                      color=0xa6192e)
                 await ctx.send(embed=embed)
                 return
-            
+
             elif source.status_code != 200:
-                embed = discord.Embed(title="Error Accessing Webpage",description=f"{source} error. Try again later", color=0xa6192e)
+                embed = nextcord.Embed(title="Error Accessing Webpage", description=f"{source} error. Try again later",
+                                      color=0xa6192e)
                 await ctx.send(embed=embed)
                 return
-            
-            soup = BeautifulSoup(source.text,'lxml')
+
+            soup = BeautifulSoup(source.text, 'lxml')
             course_description = soup.find_all('p')
-            
+
             embed_description = course_description[1].get_text() + "\n"
             course_title = soup.find_all('h1')
             course_title = str(course_title[1].get_text()).split()
-            embed_title = course_name.upper()+str(course_number).upper() + " " + section + " - "
-            for i in range(0,len(course_title) - 3):
+            embed_title = course_name.upper() + str(course_number).upper() + " " + section + " - "
+            for i in range(0, len(course_title) - 3):
                 embed_title = embed_title + course_title[i] + " "
-        
+
             try:
-                source = requests.get(f'http://www.sfu.ca/outlines.html?2021/fall/{course_name.lower()}/{course_number}/{section}',timeout=5).text
+                source = requests.get(
+                    f'http://www.sfu.ca/outlines.html?2021/fall/{course_name.lower()}/{course_number}/{section}',
+                    timeout=5).text
 
             except Timeout:
                 await ctx.send("Error accessing server data, please try again later")
 
             else:
                 soup = BeautifulSoup(source, 'lxml')
-                
+
                 time = soup.find("li", {"class": "course-times"})
                 if time is None:
-                    time = ["Course Times + Location:", "N/A"] 
+                    time = ["Course Times + Location:", "N/A"]
                 else:
-                   time = time.text.split()
-                
+                    time = time.text.split()
+
                 prereq = soup.find("li", {"class": "prereq"})
                 if prereq is None:
                     prereq = ["Prerequisites:", "N/A"]
                 else:
                     prereq = prereq.text.split()
                 instructor = soup.find("li", {"class": "instructor"})
-                
+
                 if instructor is None:
                     instructor = ["Instructor:", "N/A"]
                 else:
                     instructor = instructor.text.split()
 
-                
-                
                 for i in range(0, len(prereq)):
                     embed_description = embed_description + prereq[i] + " "
 
@@ -81,22 +86,20 @@ class Outline(commands.Cog):
 
                 for i in range(0, len(instructor)):
                     embed_description = embed_description + instructor[i] + " "
-                
+
                 embed_description = embed_description + "\n \n"
                 for i in range(0, len(time)):
                     if len(time[i]) > 2 and (time[i][0] + time[i][1] == "PM" or time[i][0] + time[i][1] == "AM"):
                         embed_description = embed_description + time[i][0] + time[i][1] + " " + time[i][2:]
                     else:
                         embed_description = embed_description + time[i] + " "
-                   
-                    
-                    if time[i] == "Location:" or time[i] == "Burnaby" or time[i] == "Surrey":
-                            embed_description = embed_description + "\n"
-                
-                embed_description = embed_description.strip(" ")
-                embed = discord.Embed(title=embed_title,description=embed_description, color=0xa6192e)
-                await ctx.send(embed=embed)
 
+                    if time[i] == "Location:" or time[i] == "Burnaby" or time[i] == "Surrey":
+                        embed_description = embed_description + "\n"
+
+                embed_description = embed_description.strip(" ")
+                embed = nextcord.Embed(title=embed_title, description=embed_description, color=0xa6192e)
+                await ctx.send(embed=embed)
 
     # If user doesn't provide zodiac sign, or enters to many parametres
     @outline.error
