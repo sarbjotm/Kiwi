5cc7a8231733d8c1942bbe91d78fbd9e276d9e18
Sarbjot Mann <sarbjotm@sfu.ca>
2022-02-01 21:15:56 -0800

    Testing what findAll does

--- cogs/horoscopes.py
+++ cogs/horoscopes.py
@@ -29,7 +29,7 @@ class Horoscope(commands.Cog):
             else:
                 soup = BeautifulSoup(source, 'lxml')
                 #today_horoscope = soup.p.text
-                paragraph = soup.find('p')
+                paragraph = soup.findAll('p')
                 today_horoscope = paragraph.get_text()
                 today_horoscope = str(today_horoscope)
                 today_horoscope = today_horoscope.replace("people", "dodos")
