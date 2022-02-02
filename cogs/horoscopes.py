913c6cd97a715a249cb6aa48734a16591d2ddcb3
Sarbjot Mann <sarbjotm@sfu.ca>
2022-02-01 21:23:44 -0800

    renamed function

--- cogs/horoscopes.py
+++ cogs/horoscopes.py
@@ -29,7 +29,7 @@ class Horoscope(commands.Cog):
             else:
                 soup = BeautifulSoup(source, 'lxml')
                 #today_horoscope = soup.p.text
-                paragraph = soup.find("div", {"class": "main-horoscope"}).findAll('p')
+                paragraph = soup.find("div", {"class": "main-horoscope"}).find_all('p')
                 today_horoscope = paragraph.get_text()
                 today_horoscope = str(today_horoscope)
                 today_horoscope = today_horoscope.replace("people", "dodos")
