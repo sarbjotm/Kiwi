9b4f82cfaecca4f1bd401544db760566636a24ef
Sarbjot Mann <44513277+sarbjotm@users.noreply.github.com>
2022-02-24 22:52:00 -0800

    Changed - to :

--- cogs/economy.py
+++ cogs/economy.py
@@ -270,7 +270,7 @@ class Economy(commands.Cog):
             position = i + 1
             username = int(leaders[i][0])
             money = str(leaders[i][1])
-            description_embed = description_embed + str(position) + ". " + "<@" + str(username) + "> " + "-" + str(money) + "\n"
+            description_embed = description_embed + str(position) + ". " + "<@" + str(username) + ">" + " : " + str(money) + "\n"
         embed = nextcord.Embed(title="Richest Dodos", color=0xe392fe)
         embed.set_thumbnail(url="https://i.imgur.com/5wjePlr.png")
         embed.add_field(name="Top 5", value=description_embed, inline=True)
